7ec3affb2846cb55cfad9737023f60a52ae688c3
marileoncio <mariana.timoteo19@yahoo.com>
2023-08-31 15:26:46 -0300

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Footer.module.css'
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <p>
+                <span>Buscador De CEP</span> @SENAI
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
