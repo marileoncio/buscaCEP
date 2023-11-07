a25678bbcc613ca45b600c094b1a63adc2d605e4
marileoncio <mariana.timoteo19@yahoo.com>
2023-11-07 10:09:26 -0300

    commit

--- src/components/listagens.tsx
+++ src/components/listagens.tsx
@@ -3,6 +3,7 @@ import React, { Component, useState, ChangeEvent, FormEvent, useEffect } from 'r
 import styles from "../App.module.css"
 import { CadastroInterface } from '../interfaces/CadastroInterface';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Listagem = () => {
     const [usuarios, setUsuarios] = useState<CadastroInterface[]>([])
@@ -102,7 +103,7 @@ const Listagem = () => {
                                             <td>{usuario.cpf}</td>
                                             <td>{usuario.email}</td>
                                             <td>
-                                                <a href="#" className='btn btn-primary btn-sm'>Editar</a>
+                                                <Link to={"/editar/"+ usuario.id} className='btn btn-primary btn-sm'>Editar</Link>
                                                 <a href="#" className='btn btn-danger btn-sm'>Excluir</a>
 
                                             </td>
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter,Route, Routes } from "react-router-dom";
 import BuscaCep from "../components/BuscaCepForm";
 import Cadastro from "../components/cadastro";
+import Editar from "../components/editar";
 import Listagem from "../components/listagens";
 
 const AppRouter = () => {
@@ -13,6 +14,7 @@ const AppRouter = () => {
           <Route path="buscacep" element={<BuscaCep />} />
           <Route path="cadastro" element={<Cadastro />} />
           <Route path="listagem" element={<Listagem />} />
+          <Route path="/editar/:id" element={<Editar />} />
         
         </Routes>
         </BrowserRouter>
